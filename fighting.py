11bb63924e46f5ea5f2e23e33038710eb5da1ac1
tastelikecoke <jmerc87@gmail.com>
2014-03-18 17:35:55 +0800

    changed physics, refactored physics, added controller

--- fighting.py
+++ fighting.py
@@ -3,7 +3,6 @@ from operator import sub,mul,add,div
 from pygame.locals import *
 white = pygame.Color(255,255,255)
 black = pygame.Color(0,0,0)
-
 # useful functions for physics
 def strictlygreater(a,b):
 	"if a tuple is strictly greater than another tuple return true else false"
@@ -13,73 +12,81 @@ def getCorners(e):
 	corner1 = map(lambda x,y:x-y/2.0, e.s, e.w)
 	corner2 = map(lambda x,y:x+y/2.0, e.s, e.w)
 	return (corner1, corner2)
-# Physics is model
-class Physics:
+class Physics: # Physics is model
 	def __init__(self,entity,kind,v,gravity):
 		"a physics component has a parent entity, a kind (floor,wall or player),v (velocity), and gravity"
 		self.entity = entity
+		self.controlled = False
+		self.onFloor = False
 		self.kind = "floor" if kind == None else kind
 		self.v = [0.0,0.0] if v == None else v
 		self.gravity = 0.0 if gravity == None else gravity
 	def update(self):
 		"update the physics component of entity"
 		self.v[1] += self.gravity
+		originals = self.entity.s
 		self.entity.s = map(add,self.entity.s, [v/30.0 for v in self.v])
+		self.v[0] *= 0.70
+		self.onFloor = False
+	def push(self, speed):
+		self.v[0] += speed
+		self.controlled = True
+	def jump(self, speed):
+		if self.onFloor:
+			self.v[1] += speed
+			self.entity.s[1] -= 1
+			self.controlled = True
+	def resolve(self, jp):
+		i = self.entity
+		ip = self
+		j = jp.entity
+		i1,i2 = getCorners(i)
+		j1,j2 = getCorners(j)
+		if strictlygreater(i2,j1) and strictlygreater(j2,i1): # enter here if two bounding boxes collide
+			if jp.kind == "floor": # enter here if target bounding box is a floor
+				self.onFloor = True
+				if j2[1] > i2[1]:
+					i.s[1] = j.s[1] - (j.w[1] + i.w[1])/2
+					ip.v[1] = 0
+				else:
+					i.s[1] = j.s[1] + (j.w[1] + i.w[1])/2
+					ip.v[1] = 0
+			else: # enter here if target bounding box is anything else
+				if j2[0] > i2[0]:
+					i.s[0] = j.s[0] - (j.w[0] + i.w[0])/2
+					ip.v[0] = 0
+					jp.v[0] = 0
+				else:
+					i.s[0] = j.s[0] + (j.w[0] + i.w[0])/2
+					ip.v[0] = 0
+					jp.v[0] = 0
 class PhysicsFactory:
 	def __init__(self):
-		"a physics factory only has the physicses"
-		self.physicses = [] 
+		"a physics factory has backdrops and players"
+		self.backdrops = []
+		self.players = [] 
 	def make(self,entity,v=None,gravity=None,kind=None):
 		"makes a new physics component for an entity"
 		p = Physics(entity,kind,v,gravity)
 		entity.physics = p
-		self.physicses.append(p)
+		if kind == "player":
+			self.players.append(p)
+		else:
+			self.backdrops.append(p)
 		return p
 	def update(self):
 		"update all physics components"
-		for p in self.physicses:
+		for p in self.players:
 			p.update()
-	def onFloor(self,entity):
-		floor = False
-		for j in xrange(len(self.physicses)):
-			spje = self.physicses[j].entity
-			if spje == entity:
-				continue
-			if spje.physics.kind == "floor":
-				spie1,spie2 = getCorners(entity)
-				spje1,spje2 = getCorners(spje)
-				floor = floor or (strictlygreater(spie2,spje1) and strictlygreater(spje2,spie1))
-		return floor
-	def resolve(self,entity1,entity2):
-		"resolves collisions of entity to all physics components"
-		spi = entity1.physics
-		spie = spi.entity
-		spj = entity2.physics
-		spje = spj.entity
-		spie1,spie2 = getCorners(spie)
-		spje1,spje2 = getCorners(spje)
-		if strictlygreater(spie2,spje1) and strictlygreater(spje2,spie1):
-			# enter here if two bounding boxes collide
-			if spj.kind == "floor":
-				# enter here if target bounding box is a floor
-				if spje2[1] > spie2[1]:
-					spie.s[1] = spje.s[1] - (spje.w[1] + spie.w[1])/2
-					spi.v[1] = 0
-				else:
-					spie.s[1] = spje.s[1] + (spje.w[1] + spie.w[1])/2
-					spi.v[1] = 0				
-			else:
-				# enter here if target bounding box is anything else	
-				if spje2[0] > spie2[0]:
-					spie.s[0] = spje.s[0] - (spje.w[0] + spie.w[0])/2					
-					spi.v[0] = 0
-					spj.v[0] = 0
-				else:
-					spie.s[0] = spje.s[0] + (spje.w[0] + spie.w[0])/2
-					spi.v[0] = 0
-					spj.v[0] = 0
-# Sprite is view
-class Sprite:
+			for b in self.backdrops:
+				p.resolve(b)
+		if self.players[1].controlled == True:
+			self.players[1].resolve(self.players[0])
+		elif self.players[0].controlled == True:
+			self.players[0].resolve(self.players[1])
+		for p in self.players:
+			p.controlled = not p.onFloor
+class Sprite: # Sprite is view
 	def __init__(self,factory,entity):
 		"a sprite has parent factory and parent entity"
 		self.factory = factory
@@ -88,10 +95,10 @@ class Sprite:
 		"draws the sprite component of entity"
 		corner1,corner2 = getCorners(self.entity)
 		position = map(add,corner1,self.factory.origin)
+		positionp = map(add,position,(-30,0))
 		if self.entity.img != None:
-			self.factory.surf.blit(self.entity.img, position+self.entity.s)
-		else:
-			pygame.draw.rect(self.factory.surf,black,position + self.entity.w,1)
+			self.factory.surf.blit(self.entity.img, positionp+self.entity.s)
+		pygame.draw.rect(self.factory.surf,black,position + self.entity.w,1)
 class SpriteFactory:
 	def __init__(self,surf,origin):
 		"a sprite factory has the main surface surf, and origin point in camera"
@@ -108,6 +115,24 @@ class SpriteFactory:
 		"draws all sprites"
 		for s in self.sprites:
 			s.draw()
+class Controller:
+	def __init__(self, dictionary):
+		self.dictionary = dictionary
+		self.presses = []
+	def handle(self, events):
+		for event in events:
+			if event.type == QUIT:
+				pygame.quit()
+				sys.exit()
+			if event.type == KEYDOWN:
+				self.presses.append(event.key)
+			if event.type == KEYUP:
+				self.presses.remove(event.key)
+		for p in self.presses:
+			try:
+				self.dictionary[p]()
+			except KeyError:
+				pass
 # Entity is mvc
 class Entity:
 	def __init__(self,img=None,s=None,w=None):
@@ -144,62 +169,21 @@ def __main__():
 	sf.make(floor)
 	sf.make(wall1)
 	sf.make(wall2)
+	controller = Controller({
+		K_LEFT: (lambda: player1.physics.push(-20.0)),
+		K_RIGHT: (lambda: player1.physics.push(20.0)),
+		K_UP: (lambda: player1.physics.jump(-300.0)),
+		K_a: (lambda: player2.physics.push(-20.0)),
+		K_d: (lambda: player2.physics.push(20.0)),
+		K_w: (lambda: player2.physics.jump(-300.0)),}
+		)
 	# game loop
 	while True:
 		surf.fill(white)
 		sf.draw()
 		pygame.display.update()
 		pf.update()
-		for event in pygame.event.get():
-			if event.type == QUIT:
-				pygame.quit()
-				sys.exit()
-			# controller for players
-			movingplayer = 0
-			if event.type == KEYDOWN:
-				if event.key == K_LEFT:
-					player1.physics.v[0] = -100.0	
-					movingplayer = 1
-				if event.key == K_RIGHT:
-					player1.physics.v[0] = 100.0
-					movingplayer = 1
-				if event.key == K_a:
-					player2.physics.v[0] = -100.0
-					movingplayer = 2 if movingplayer != 1 else 0
-				if event.key == K_d:
-					player2.physics.v[0] = 100.0
-					movingplayer = 2 if movingplayer != 1 else 0
-				if event.key == K_UP:
-					if pf.onFloor(player1):
-						player1.physics.v[1] = -300.0
-						player1.s[1] -= 1
-				if event.key == K_w:
-					if pf.onFloor(player2):
-						player2.physics.v[1] = -300.0
-						player2.s[1] -= 1
-			if event.type == KEYUP:
-				if event.key == K_LEFT:
-					player1.physics.v[0] = 0.0
-				if event.key == K_RIGHT:
-					player1.physics.v[0] = 0.0
-				if event.key == K_a:
-					player2.physics.v[0] = 0.0
-				if event.key == K_d:
-					player2.physics.v[0] = 0.0
-		if movingplayer == 0:
-			if not pf.onFloor(player1) and pf.onFloor(player2):
-				movingplayer = 1			
-			elif pf.onFloor(player1) and not pf.onFloor(player2):
-				movingplayer = 2
-		if movingplayer == 1:
-			pf.resolve(player1, player2)
-		elif movingplayer == 2:
-			pf.resolve(player2, player1)
-		else:
-			pf.resolve(player1, player2)
-		for e in [floor,wall1,wall2]:
-			pf.resolve(player1, e)
-			pf.resolve(player2, e)
+		controller.handle(pygame.event.get())
 		fpsClock.tick(60)
 
 __main__()
